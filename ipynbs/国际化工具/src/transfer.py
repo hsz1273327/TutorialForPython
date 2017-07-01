52350f36a40fe4870b664e7725ca38483539415c
HUANG SIZHE <hsz1273327@gmail.com>
2017-07-01 09:06:14 +0800

    update
    update

---
+++ ipynbs/国际化工具/src/transfer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import gettext
+langen = gettext.translation('en', './src/locale', languages=['en'])
+langcn = gettext.translation('zh_CN', './src/locale', languages=['cn'])
