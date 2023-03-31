227bbc0330d136cbd4ca41ebc354ee4631944c99
HUANG SIZHE <hsz1273327@gmail.com>
2023-03-31 15:56:38 +0800

    update

---
+++ src/基础应用篇/国际化/src/gettext_te.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from transfer import *
+langcn.install()
+print(_("Hello world!"))
+langen.install()
+print(_("Hello world!"))
